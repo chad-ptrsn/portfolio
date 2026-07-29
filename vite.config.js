f7d2dfc72dc6c9f85e75529d86fe80a706f766cd
Chad Petersen <chadpetersen@Chads-MacBook-Pro.local>
2026-07-29 15:21:36 -0500

    Add Cloudflare Pages configuration and improve Vite build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000,
-    open: true
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
